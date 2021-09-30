99e61250f092f7cd6dda6d74b202dfb8d751aeec
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-09-30 20:25:12 +1000

    rehauled button css behaviour, added dom-interactivity js file

--- javaScript/calculations.js
+++ javaScript/calculations.js
@@ -1,14 +1,2 @@
-const backButton = document.querySelector(".calculator__button--back");
-const svg = document.querySelector(".svg");
 
-const hoverOn = () => {
-  svg.style.fill = "grey"
-  return;
-}
-const hoverOff = () => {
-  svg.style.fill = "#c7c8ca"
-  return;
-}
-
-backButton.addEventListener("mouseover", hoverOn);
-backButton.addEventListener("mouseout", hoverOff);
+//
