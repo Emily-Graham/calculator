a877320fc6e0bed23536d35966614da719ae6d3c
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-10-01 15:53:01 +1000

    all buttons input corrosponding value when pressed, call appropriate functions

--- javaScript/calculations.js
+++ javaScript/calculations.js
@@ -1,20 +1,20 @@
 
 //additon 
-export const addition = (a, b) => {
+const addition = (a, b) => {
   return a + b;
 }
 
 //subtraction
-export const subtraction = (a, b) => {
+const subtraction = (a, b) => {
   return a - b;
 }
 
 //multiplication
-export const multiplication = (a, b) => {
+const multiplication = (a, b) => {
   return a * b;
 }
 
 //divide
-export const divide = (a, b) => {
+const divide = (a, b) => {
   return a / b;
 }
--- javaScript/dom-interactivity.js
+++ javaScript/dom-interactivity.js
@@ -1,10 +1,8 @@
-import * as cal from "./calculations.js"; 
-
-//DOM selection
-const backButton = document.querySelector(".calculator__button--back");
+//GLOBAL VARIABLES
+const deleteButton = document.querySelector(".calculator__button--delete");
 const svg = document.querySelector(".svg");
 
-//button pressed behaviour
+//CSS BUTTON PRESSED BEHAVIOUR
 const active = () => {
   svg.style.fill = "#FFF"
 }
@@ -12,41 +10,113 @@ const notActive = () => {
   svg.style.fill = "#b0b2b4"
 }
 
-//each button needs to have an equvilant value
-//when numeral buttons pressed, value is appended to currentCalculation
-  //if currentCalculation value is 0, replace with new value
-  //for every numeral exceeding a group of 3, add a ',' to displayed text
 
-//when operator buttons are pressed 
+
+
+//CLEAR PRESSED
+const clear = () => {
+  console.log("Clear has been clicked!");
+}
+  //currentCalculation equates to 0
+
+
+
+//BACKWARDS PRESSED
+const deleteBackwards = () => {
+  console.log("Delete backwards whas been clicked!");
+}
+//Back removes the last numeral/operator in previousCal
+  //if an operator, also remove space in display text
+  //if there is only one numeral in currentCalculation, replace with default zero
+
+
+
+  //OPERATOR PRESSED
+const operator = (value) => {
+  console.log(`I'm working because ${value} has been pressed!`);
+}
   //any currentCalculation value is sent to previousCalculation
     //if there is a previous value and an operator, it is calculated
       //resulting value sent to currentCalculation
   //operator appends to previousCalculation, with space between
     //if there is no previous value, operator is appended to the default 0
     //if an operation is currently at end of previousCalculation, replace it with new operator
-    
-//Clear clears all stored data
-  //currentCalculation equates to 0
 
-//Back removes the last numeral/operator in previousCal
-  //if an operator, also remove space in display text
-  //if there is only one numeral in currentCalculation, replace with default zero
 
-//equals resolves equation and displays result in currentCalculation
-  //if previousCalculation does not end in an operator, accumalate last operator with each additional click
-  //if previousCalculation ends in an decimal point, delete decimal
 
-//decimal adds a decimal point
+//DECIMAL PRESSED
+const appendDecimal = () => {
+  console.log("A decimal should get added!");
+}
   //if last value is a decimal, do not add
   //if last value is an operator, first add 0
+  //if no previous value, first add 0
+  //else, append decimal
 
-  //0 button pressed
+
+
+//NUMERICAL BUTTON PRESSED
+const appendNumeral = (value) => {
+  console.log(`the number ${value} got pressed!`);
+}
+//0 button pressed
     //if there is only one value, do not add if also 0
 
 
 
+//EQUALS PRESSED
+const resolveExpression = () => {
+  console.log("I should resolve the expression thus far!");
+}
+//equals resolves equation and displays result in currentCalculation
+  //if previousCalculation does not end in an operator, accumalate last operator with each additional click
+  //if previousCalculation ends in an decimal point, delete decimal
 
 
-//event listeners
-backButton.addEventListener("mousedown", active);
-backButton.addEventListener("mouseup", notActive);
+
+//BUTTON PRESSED
+//when numeral buttons pressed, value is appended to currentCalculation
+const buttonPressed = (input) => {
+  //connect button pressed to corrosponding value
+  console.log(input);
+  //call corrosponding function
+  switch (input) {
+    case "/": 
+      operator(input);
+      break;
+    case "*":
+      operator(input);
+      break;
+    case "-": 
+      operator(input);
+      break;
+    case "+":
+      operator(input);
+      break;
+    case "C":
+      clear();
+      break;
+    case "D":
+      deleteBackwards();
+      break;
+    case ".":
+      appendDecimal();
+      break;
+    case "=":
+      resolveExpression();
+      break;
+    default:
+      appendNumeral(input);
+      break;
+  }
+}
+  //if currentCalculation value is 0, replace with new value
+  //for every numeral exceeding a group of 3, add a ',' to displayed text
+
+
+
+//EVENT LISTENERS
+deleteButton.addEventListener("mousedown", active);
+deleteButton.addEventListener("mouseup", notActive);
+  //calls buttonPressed function
+document.querySelectorAll(".calculator__button").forEach((button) => button.addEventListener("click", (event) => buttonPressed(event.target.id)));
